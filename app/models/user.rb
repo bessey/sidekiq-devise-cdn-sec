95f0f40d518f2591a9078bddbe4f3bafaa8821aa
Matt Bessey <matt.bessey@immersivelabs.com>
2023-06-05 17:46:36 +0100

    Minimal repro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable
+  devise :database_authenticatable
 end
